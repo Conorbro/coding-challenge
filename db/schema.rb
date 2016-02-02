feb639566f431b01c52c92c0422e362f7bee2e1b
conorbroderick <cjayjayb@gmail.com>
2016-02-02 22:55:50 +0000

    Deploying factors for Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,6 @@ ActiveRecord::Schema.define(version: 20160202154440) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "tags"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
